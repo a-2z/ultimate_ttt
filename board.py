d8d04a7cf3efcd69493a4ffe56f4d3c43370199c
az359 <az359@cornell.edu>
2021-04-11 23:21:08 -0400

    skeleton in development

---
+++ board.py
@@ -0,0 +1,24 @@
+import numpy as npclass Board:
+
+    def __init__(self):
+        """
+        Class constructor for a single tic-tac-toe board.
+        """
+        self.empty = np.zeros((3, 3))
+
+        # X is True, O is false
+        self.turn = True
+
+    def _print_board(self):
+        """
+        Prints a single tic-tac-toe board given the current game state.
+        """
+
+  f _move(self, cell: int*int) -> bool:
+        """tupleesagilstuple
+https://docs.python.org/3/library/typing.html 
+        Sets an entry in the matrix 
+                """
+        pass
+
+      https://docs.python.org/3/library/typing.html
---
+++ main.py
@@ -0,0 +1,40 @@
+
+# Basic setup
+import numpy as np
+import board
+
+
+def main():
+    pass
+
+
+class TicTacToe:
+    def __init__(self):
+        """
+        Class constructor for a game of Ultimate Tic-Tac-Toe
+        """
+        self.empty = np.zeros((3, 3))
+        self.turn = True
+
+    def display_board(self):
+        """
+        Prints the main tic-tac-toe-board given the current game state
+        """
+        pass
+
+    def _move(self, cell: int) -> bool:
+        """
+
+        """
+        pass
+
+    def check_win():
+        """
+        Checks to see if a cell (one of the 3x3 boards) has been won.
+
+        Returns True if a cell has been one; otherwise
+        """
+
+
+if __name__ == "__main__":
+    main()
