d661c2e51b49849dbc6a50243a1d4c55bfea64ce
Andrew <droo@localhost.localdomain>
2021-04-27 01:52:54 -0400

    random simulation

--- ai.py
+++ ai.py
@@ -1,144 +1,114 @@
-#tree class for state
-import numpy as np
-import re
 from board import UltimateTTT, State
-
-#rotation angles based on the first non-central moves
-orientations = {
-    (0, 0): np.radians(-90),
-    (0, 1): None,
-    (0, 2): None,
-    (1, 0): np.radians(-90),
-    (1, 2): np.radians(90),
-    (2, 0): np.radians(180),
-    (2, 1): np.radians(180),
-    (2, 2): np.radians(90)
-}
+from math import log, sqrt
+import random
+import copy
 
 class MoveNode:
-    def __init__(self, parent):
-        self.children = set()
+    """
+    Represents a move in an ultimate Tic-Tac-Toe game.
+    When ancestors are taken into account, the node
+    represents the history/state of a Tic-Tac-Toe game thus far.
+    """
+    def __init__(self, move, parent = None, children = set(), ucb_c = 2):
+        """
+        Initializes a move node in a decision tree.
+
+        By default, the node is a root.
+        """
         self.parent = parent
-        self.moves = []
-        self.ucb = 0
-        self.n_i = 0
+        self.children = children
+        self.move = move
+        self.n = 0
         self.wins = 0
-        self.value = None
+        #a hyperparameter used for computing the UCB
+        self.c = ucb_c
 
-    def insert(self):
-        pass 
-
-    def lookup(self, history):
-        pass
-
-class Player:
-    def __init__(self, turn, num_iters):
-        self.game = UltimateTTT()
-        #canonical orientations
-        self.orientation_set = False
-        self.rotate = True
-        self.rot_sin, self.rot_cos = 0, 0
-        #decision tree for AI
-        self.decision_tree = MoveNode()
-
-####################Rotation########################### 
-    def rotate(self, move):
+    def compute_ucb(self):
         """
-        rotates a move given in (global_coords, local_coords)
-        to some canonical direction given by the first
-        non-central move
-        """
-        if not self.rotate:
-            return move
-        glob_x, loc_x = move[0][0] - 1, move[1][0] - 1
-        glob_y, loc_y = move[0][1] - 1, move[1][1] - 1
+        Returns the upper confidence bound
+        of a MoveNode. 
 
-        #rotate the indices in the x
-        glob_x_prime = -glob_y * self.rot_sin + glob_x * self.rot_cos
-        loc_x_prime = -loc_y * self.rot_sin + loc_x * self.rot_cos
-        #rotate the indices in the y
-        glob_y_prime = glob_y * self.rot_cos + glob_x * self.rot_sin
-        loc_y_prime =  loc_y * self.rot_cos + loc_x * self.rot_sin
-
-        #reconstruct the move
-        new_glob = (round(glob_x_prime + 1), round(glob_y_prime + 1))
-        new_loc = (round(loc_x_prime + 1), round(loc_y_prime + 1))
-        return ((new_glob, new_loc))
-        
-    def set_orientations(self, move):
+        Precondition: The node is not the root node.
         """
-        Sets the canonical orientation of the board
-        based on the first non-center move on the board.
+        #root and unsimulated moves have infinite UCB
+        if not self.parent or self.n = 0:
+            return float('inf')
+        x_i = self.wins/self.n
+        return x_i + c * sqrt(log(self.parent.n)/self.n)
 
-        This way, variations (rotations) of the same state
-        will be combined.
+    def expand(self, candidates):
+        self.children = [MoveNode(move=c, parent=self) for c in candidates]
+
+    def back_propagate(self, result):
+        curr = self 
+        while curr:
+            curr.n += 1
+            curr.wins += result 
+            curr = curr.parent
+
+class MCTS:
+    """
+    Implements playing of Ultimate Tic-Tac-Toe using 
+    Monte Carlo Tree Search
+    """
+    #The number of iterations to run the algorithm based on the difficulty
+    DIFFICULTY = {level: level * 100000 for level in range(1, 6)}
+
+    def __init__(self, difficulty = 5):
+        #hyperparameter for computing upper confidence bounds
+        self.c = 2
+        self.max_iters = DIFFICULTY[difficulty]
+        self.game = None
+
+    def pick_move(self, game):
         """
-        if move[0] == (1, 1):
-            if move[1] == (1, 1): 
-                return
+        Runs MCTS for a number of iterations based on difficulty and selects
+        the optimal move to play from the current position.
+
+        Returns the move (global_coord, local_coord) corresponding to the 
+        highest UCB estimate.
+        """
+        self.game = game
+        sim_game = copy.deepcopy(self.game)
+        root = MoveNode(self.game.moves[-1])
+        candidates = self.game.available_moves()
+        root.children = root.expand(candidates)
+        run_sims(root)
+
+    def run_sims(self, root):
+        game_tmp = copy.deepcopy(self.game)
+        next_move = max(root.children, lambda c: c.compute_ucb())
+
+        for _ in self.max_iters:
+            if next_move.n == 0:
+                result = self.play_random(game_tmp)
+                next_move.back_propagate(result)
             else:
-                angle = orientations[move[1]]
-                if angle is None:
-                    self.no_rotate = True
-                    return
-                self.rot_sin = np.sin(angle)
-                self.rot_cos = np.cos(angle)
-                self.orientation_set = True
+                game_tmp.move(next_move)
+                candidates = game_tmp.available_moves()
+                next_move.expand(candidates)
+                next_move = max(next_move.children, lambda c: c.compute_ucb())
+                game_tmp.move()
+                    
+    def expand(self, node, candidates):
+        node.children = [MoveNode(move=c, parent=next_move) for c in candidates]
+
+    def play_random(self, game):
+        """
+        Returns the outcome of a game in which only random moves have been 
+        played for both players
+        """
+        while game.result == INCOMPLETE:
+            candidates = game.available_moves
+            game.move(random.choice(candidates))
+        if game.result.value == 1:
+            return 1 
+        #draw
+        elif game.result.value == -2:
+            return 0.5
         else:
-            angle = orientations[move[0]]
-            if angle is None:
-                    self.no_rotate = True
-                    return
-            self.rot_sin = np.sin(angle)
-            self.rot_cos = np.cos(angle)
-            self.orientation_set = True
+            return 0
 
-####################Play a game###########################
-    def play(self, to_play = 1):
-        root = MoveNode(None)
-        while self.game.result == State.INCOMPLETE:
-            if to_play == 1:
-                print(str(self.game))
-                p_move = parse_input(input("Move: "))
-                self.game.move(p_move)
-                
 
-            next_move = self.simulate(self.game.moves)
-            if self.rotate and not self.orientation_set:
-                self.set_orientations(next_move)
-            rot_move = self.rotate(next_move)
-            self.game.move(next_move)
-    
-    def get_p_move(self):
 
-    def parse_input(str):
-        parsed = re.findall("[0-9]+", str)
-        return ((int(parsed[0]), int(parsed[1])), (int(parsed[2]), int(parsed[3])))
-
-    def simulate(self, history):
-        """
-        Runs simulations of an ultimate Tic-Tac-Toe game for
-        num_iters and returns the move with the highest 
-        upper confidence bound.
-        """
-        root = MoveNode(history[0])
-
-# get available moves - make tree from that
-# pick random move initially - ucb all set to 0
-# keep picking random moves and expanding child nodes
-# 
-#play all moves in sequence until arrive at root
-#orientation: -90
-#0, 0, 1, 1
-#root = MoveNode(None, history[0])
-# increment n_i for root
-#self.unexplored = self.game.available_moves()
-#for i in unexplored:
-#   self.children.add(MoveNode(self, move)   
-
-#
-#
-#
-            
-    
 
